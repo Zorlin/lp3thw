ca669400a0d061c998230c6ef492a8c2747bfa59
Benjamin Arntzen <zorlin@gmail.com>
2017-07-31 22:36:14 +0800

    Complete ex1 + ex2

---
+++ exercises/ex1-ec2.py
@@ -0,0 +1,13 @@
+#ec1.2
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+print("Only one of the lines!")
+
+#ec1.3
+# Octothorpe is used to create comments which don't run as code.
