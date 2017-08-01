9a5867aea7d0df47891ab5ff7b7add9d2eab3b79
Benjamin Arntzen <zorlin@gmail.com>
2017-08-01 14:01:28 +0800

    Added extra credit for ex5.

---
+++ exercises/ex5-ec.py
@@ -0,0 +1,26 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+# Number of centimers in an inch
+centimeters_per_inch = 2.54
+kilograms_per_pound = 0.453592
+
+print(f"He's {height * centimeters_per_inch } centimeters tall.")
+print(f"He's {weight * kilograms_per_pound } kilograms heavy.")
+
